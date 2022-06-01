5fde92dd35c31d88d355ea5613f603bd1129c162
Jessica Blanton <jbblanton@dgtech.com>
2022-06-01 09:36:02 -0400

    June!

---
+++ 06012022.js
@@ -0,0 +1,108 @@
+// Day 27
+//*******************************
+
+// Kata: Removing Elements
+
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+function removeEveryOther(arr) {
+  return arr.filter((ele, idx) => idx % 2 === 0);
+}
+
+/* ----------------------------------------------------------------------- */
+
+// Kata: Sum without highest and lowest number
+
+// Task
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// Input validation
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+function sumArray(array) {
+  // array.length 2 or less? return 0
+  // splice out the max and min
+  // return reduce()
+
+  if (!array || array.length < 3) {
+    return 0;
+  }
+
+  // Find the max location
+  // Remove them
+  let max = array.indexOf(Math.max(...array));
+  array.splice(max, 1);
+
+  // Find the min location
+  // Remove them
+  let min = array.indexOf(Math.min(...array));
+  array.splice(min, 1);
+
+  // Reduce and return the remaining items
+  return array.reduce((sum, num) => (sum += num), 0);
+}
+
+/* ----------------------------------------------------------------------- */
+
+// Kata:  Volume of a Cuboid
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
+
+/* ----------------------------------------------------------------------- */
+
+// Kata: Student's Final Grade
+
+// Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+// This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+// This function should return a number (final grade). There are four types of final grades:
+
+// 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+// 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+// 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+// 0, in other cases
+// Examples(Inputs-->Output):
+
+// 100, 12 --> 100
+// 99, 0 --> 100
+// 10, 15 --> 100
+
+// 85, 5 --> 90
+
+// 55, 3 --> 75
+
+// 55, 0 --> 0
+// 20, 2 --> 0
+// *Use Comparison and Logical Operators.
+
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) {
+    return 100;
+  } else if (exam > 75 && projects >= 5) {
+    return 90;
+  } else if (exam > 50 && projects >= 2) {
+    return 75;
+  } else {
+    return 0;
+  }
+}
