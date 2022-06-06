af64a3739638faf4358e965a58f94cf42dd2a9e4
Jessica Blanton <jbblanton@dgtech.com>
2022-06-06 09:19:07 -0400

    June 6

---
+++ 06062022.js
@@ -0,0 +1,157 @@
+// Day 30
+//*******************************
+
+// Kata: Enumerable Magic #25 - Take the First N Elements
+
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+function take(arr, n) {
+  return arr.slice(0, n);
+}
+
+/* ----------------------------------------------------------------------- */
+
+// Kata: Sort Numbers
+
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+
+function solution(nums) {
+  if (!nums) {
+    return [];
+  }
+
+  return nums.sort((a, b) => a - b);
+}
+
+/* ----------------------------------------------------------------------- */
+
+// Kata: Exclusive "or" (xor) Logical Operator
+
+// In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" (hence the name of this Kata). The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise. For example:
+
+// false xor false == false // since both are false
+// true xor false == true // exactly one of the two expressions are true
+// false xor true == true // exactly one of the two expressions are true
+// true xor true == false // Both are true.  "xor" only returns true if EXACTLY one of the two expressions evaluate to true.
+// Task
+// Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
+
+function xor(a, b) {
+  return (a && !b) || (!a && b);
+}
+
+/* ----------------------------------------------------------------------- */
+
+// Kata: Multiplication table for number
+
+// Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+// For example, a multiplication table (string) for number == 5 looks like below:
+
+// 1 * 5 = 5
+// 2 * 5 = 10
+// 3 * 5 = 15
+// 4 * 5 = 20
+// 5 * 5 = 25
+// 6 * 5 = 30
+// 7 * 5 = 35
+// 8 * 5 = 40
+// 9 * 5 = 45
+// 10 * 5 = 50
+// P. S. You can use \n in string to jump to the next line.
+
+// Note: newlines should be added between rows, but there should be no trailing newline at the end. If you're unsure about the format, look at the sample tests.
+
+function multiTable(number) {
+  let math = "";
+
+  for (let i = 1; i < 10; i++) {
+    math += `${i} * ${number} = ${i * number}\n`;
+  }
+  math += `10 * ${number} = ${10 * number}`;
+
+  return math;
+}
+
+/* ----------------------------------------------------------------------- */
+
+// Kata: Get character from ASCII Value
+
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Example: get_char(65)
+// should return: 'A'
+
+// For ASCII table, you can refer to http://www.asciitable.com/
+
+function getChar(c) {
+  return String.fromCharCode(c);
+}
+
+/* ----------------------------------------------------------------------- */
+
+// Kata: Your order, please
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+  // if empty, return empty
+  // digits are 1-9
+  //    .includes?
+  //    split string to list, then can inspect eles
+
+  if (!words) {
+    return words;
+  }
+
+  let ordered = [];
+  let splitWords = words.split(" ");
+
+  for (let i = 1; i < splitWords.length + 1; i++) {
+    let next = splitWords.filter((word) => word.includes(i));
+    ordered.push(next);
+  }
+
+  return ordered.join(" ");
+}
+
+/* ----------------------------------------------------------------------- */
+
+// Kata:  Two fighters, one winner.
+
+// Create a function that returns the name of the winner in a fight between two fighters.
+
+// Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+// Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+// Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+// Example:
+//   declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+
+//   Lew attacks Harry; Harry now has 3 health.
+//   Harry attacks Lew; Lew now has 6 health.
+//   Lew attacks Harry; Harry now has 1 health.
+//   Harry attacks Lew; Lew now has 2 health.
+//   Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+// function Fighter(name, health, damagePerAttack) {
+//         this.name = name;
+//         this.health = health;
+//         this.damagePerAttack = damagePerAttack;
+//         this.toString = function() { return this.name; }
+// }
